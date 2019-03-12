6aa31fc94dbef95cec799131a6766a0ada9b83aa
novellito <novellito7@gmail.com>
2019-03-11 23:10:33 -0700

    update container hoc

--- src/containers/ContainerHOC.js
+++ src/containers/ContainerHOC.js
@@ -2,6 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
+  @media (min-width: 576px) {
+    .container {
+      max-width: 540px;
+    }
+  }
+  @media (min-width: 768px) {
+    .container {
+      max-width: 720px;
+    }
+  }
   @media (min-width: 992px) {
     .container {
       max-width: 960px;
