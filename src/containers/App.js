b38d7c537364c174a9e774b2a22a576e9d6d583c
novellito <novellito7@gmail.com>
2021-09-02 18:07:41 -0700

    revert route

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,32 +1,14 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
-import wip from '../assets/wip.svg';
-import resume from '../assets/christianTrinidad_Resume.pdf';
-
-const Wrapper = styled.section`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  .text {
-    padding: 0 40px;
-  }
-`;
-
+import { Route, Switch } from 'react-router-dom';
+import Root from './Root';
 class App extends Component {
   render() {
     return (
-      <Wrapper>
-        <div className="text">
-          <h1>Hi there! 👋 </h1>
-          <p>
-            My site is being updated but you can still find my latest résumé{' '}
-            <a href={resume} target="_blank" rel="noopener noreferrer">
-              here
-            </a>
-          </p>
-        </div>
-        <img className="wip" alt="site is being updated" src={wip} />
-      </Wrapper>
+      <>
+        <Switch>
+          <Route exact path="/" component={Root} />
+        </Switch>
+      </>
     );
   }
 }
--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-import Hero from '../components/Hero';
-import About from '../components/About';
-import Experience from '../components/Experience';
-import Skills from '../components/Skills';
+import styled from 'styled-components';
+import wip from '../assets/wip.svg';
+import resume from '../assets/christianTrinidad_Resume.pdf';
+
+const Wrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  .text {
+    padding: 0 40px;
+  }
+`;
 
 class Root extends Component {
   render() {
     return (
       <>
-        <Hero />
-        <About />
-        <Experience />
-        <Skills />
+        <Wrapper>
+          <div className="text">
+            <h1>Hi there! 👋 </h1>
+            <p>
+              My site is being updated but you can still find my latest résumé{' '}
+              <a href={resume} target="_blank" rel="noopener noreferrer">
+                here
+              </a>
+            </p>
+          </div>
+          <img className="wip" alt="site is being updated" src={wip} />
+        </Wrapper>
       </>
     );
   }
