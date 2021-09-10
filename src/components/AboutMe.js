de73936af5e710c187fbfcbecab71e77433e0234
novellito <novellito7@gmail.com>
2021-09-10 13:44:04 -0700

    add carousel to about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { styled } from 'twin.macro';
 import { StaticImage } from 'gatsby-plugin-image';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-const imageDimensions = '170px';
+const imageDimensions = '150px';
+
 const AboutMeContainer = styled.section`
   background-color: #282a36;
   padding: 30px 70px;
@@ -16,17 +19,24 @@ const AboutMeConent = styled.div`
     color: hsl(0deg 0% 80%);
     line-height: 1.4rem;
   }
-  img {
-    width: ${imageDimensions};
-    height: ${imageDimensions};
-    border-radius: 50%;
-  }
 
   @media (min-width: 768px) {
     display: grid;
     align-items: center;
     justify-items: center;
     grid-template-columns: 1fr 2fr;
+  }
+
+  .carousel-root {
+    display: flex;
+    justify-content: center;
+    .carousel .slide {
+      img {
+        width: ${imageDimensions};
+        height: ${imageDimensions};
+        border-radius: 50%;
+      }
+    }
   }
 `;
 const AboutMe = () => {
@@ -35,11 +45,32 @@ const AboutMe = () => {
       <AboutMeConent className="main">
         <div tw="text-center">
           <h1 tw="text-center md:hidden">Who Am I?</h1>
-          <StaticImage
-            src="../images/xtian.jpg"
-            alt="picture of me"
-            placeholder="blurred"
-          />
+          <Carousel
+            showArrows={false}
+            showStatus={false}
+            showIndicators={false}
+            infiniteLoop={true}
+            autoPlay={true}
+            showThumbs={false}
+            swipeable={false}
+            width={imageDimensions}
+          >
+            <StaticImage
+              src="../images/me1.jpg"
+              alt="a picture of me"
+              placeholder="blurred"
+            />
+            <StaticImage
+              src="../images/me2.jpg"
+              alt="a picture of me"
+              placeholder="blurred"
+            />
+            <StaticImage
+              src="../images/me3.jpg"
+              alt="a picture of me"
+              placeholder="blurred"
+            />
+          </Carousel>
         </div>
         <div className="about-text">
           <h1 tw="hidden md:block text-left">Who Am I?</h1>
