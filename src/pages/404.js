1bb2c0da9e3694c93850df0e3619dc1d3a217501
novellito <novellito7@gmail.com>
2021-09-09 23:25:55 -0700

    update 404 page :)

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,37 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from 'react';
+import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
+import { styled } from 'twin.macro';
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+const NotFoundContainer = styled.main`
+  color: #232129;
+  padding: 96px;
+  font-family: -apple-system, Roboto, sans-serif, serif;
+  text-align: center;
+  h1 {
+    margin: 0;
+  }
+`;
 
 // markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
+    <>
       <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+      <NotFoundContainer>
+        <h1>Page not found</h1>
+        <StaticImage
+          src="../images/404.png"
+          alt="picture of me"
+          placeholder="blurred"
+        />
+        <p>
+          Sorry 😔 we couldn’t find what you were looking for.
+          <br /> <Link to="/">Go back home</Link>.
+        </p>
+      </NotFoundContainer>
+    </>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
