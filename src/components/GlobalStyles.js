450f33bbdeec0f4e6beeeaf2af26701c069158a9
novellito <novellito7@gmail.com>
2021-09-17 09:27:19 -0700

    format global styles

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -14,62 +14,62 @@ body {
   font-family: 'Lato', sans-serif;
 }
 
-body, html, #___gatsby,
+body,
+html,
+#___gatsby,
 #gatsby-focus-wrapper {
-    height: 100%;
-    margin: 0px;
-    padding: 0px;
-    transition: all 0.50s linear;
+  height: 100%;
+  margin: 0px;
+  padding: 0px;
+  transition: all 0.5s linear;
     background-color: ${({ theme }) => theme.body};
 }
 
 h1 {
     color:${({ theme }) => theme.h1};
-    transition: all 0.50s linear;
+    transition: all 0.5s linear;
 }
 #navlinks {
-    a {
-        color:${({ theme }) => theme.navlinks};
-        transition: all 0.50s linear;
-
-    }
+  a {
+    color:${({ theme }) => theme.navlinks};
+    transition: all 0.50s linear;
+  }
 }
 
 #about {
     background:${({ theme }) => theme.aboutBg};
-    transition: all 0.50s linear;
-    p {
-        color:${({ theme }) => theme.aboutMeParagraph};
-        transition: all 0.50s linear;
-    }
-
+    transition: all 0.5s linear;
+  p {
+    color:${({ theme }) => theme.aboutMeParagraph};
+    transition: all 0.5s linear;
+  }
 }
 
 #experience {
     background: ${({ theme }) => theme.experienceBG};
-    transition: all 0.50s linear;
+    transition: all 0.5s linear;
     p,
     .vertical-timeline-element-title,
     .vertical-timeline-element-subtitle,
     .vertical-timeline-element-date {
         color: ${({ theme }) => theme.paragraph};
-        transition: all 0.50s linear;
+        transition: all 0.5s linear;
     }
-    
-    .vertical-timeline-element-content {
+
+  .vertical-timeline-element-content {
         background-color: ${({ theme }) =>
           theme.experienceCardBackground}!important;
-    }
+  }
 }
 
 #skills {
- .skills-card {
-    background: ${({ theme }) => theme.skillsCardBG};
+  .skills-card {
+        background: ${({ theme }) => theme.skillsCardBG};
     p {
-        color: ${({ theme }) => theme.paragraph}; 
-        transition: all 0.50s linear;
+        ${({ theme }) => theme.paragraph};
+        transition: all 0.5s linear;
     }
- }
+  }
 }
 
 #aws-logo {
@@ -77,43 +77,44 @@ h1 {
 }
 
 #projects {
-    .card {
-        background: ${({ theme }) => theme.skillsCardBG};
-    }
-    .project-name, p {
-        color: ${({ theme }) => theme.paragraph}; 
-        transition: all 0.50s linear;
-    }
+  .card {
+    background: ${({ theme }) => theme.skillsCardBG};
+  }
+  .project-name,
+  p {
+    color: ${({ theme }) => theme.paragraph};
+    transition: all 0.5s linear;
+  }
 }
 
 footer {
     background: ${({ theme }) => theme.footerBG};
-    transition: all 0.50s linear;
+    transition: all 0.5s linear;
 }
 
 #mobile-nav {
-    .mobile-menu {
+  .mobile-menu {
         background: ${({ theme }) => theme.mobileBGColor};
-        
-        button, 
-        .circle { 
-            background: ${({ theme }) => theme.mobileBGColor};
-        }
-        
-        ul li a {
-            color: ${({ theme }) => theme.mobileHamburger};
-        }
-        
-        .line {
-            span {
-                background: ${({ theme }) => theme.mobileHamburger};
-            }
-        }
-        
-        &.open span {
-            background: #DC2626;
-        }
+
+    button,
+    .circle {
+        background: ${({ theme }) => theme.mobileBGColor};
     }
+
+    ul li a {
+        color: ${({ theme }) => theme.mobileHamburger};
+    }
+
+    .line {
+      span {
+        background: ${({ theme }) => theme.mobileHamburger};
+      }
+    }
+
+    &.open span {
+      background: #dc2626;
+    }
+  }
 }
 
 .avatar-shoes {
@@ -123,4 +124,5 @@ footer {
 .theme-toggler {
     background: ${({ theme }) => theme.mobileBGColor};
 }
+
 `;
