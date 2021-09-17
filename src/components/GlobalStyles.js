60a519f0f9990181113e4020f59a72c43af58429
novellito <novellito7@gmail.com>
2021-09-16 23:50:16 -0700

    test remove overflow x

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -21,7 +21,7 @@ body, html, #___gatsby,
     padding: 0px;
     transition: all 0.50s linear;
     background-color: ${({ theme }) => theme.body};
-    overflow: hidden;
+    overflow-x: hidden;
 }
 
 h1 {
