d6b473fe9ddf88e8c0322a56c8d3bbc7ec857cca
novellito <novellito7@gmail.com>
2022-09-03 10:33:05 -0700

    fix import typo

--- src/components/ui/HeroLinks.tsx
+++ src/components/ui/HeroLinks.tsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import styles from '@/styles/herolinks.module.scss';
+import styles from '@/styles/HeroLinks.module.scss';
 import { HeroLink } from '@/constants/types';
 
 const HeroAnchor = (props: HeroLink) => {
